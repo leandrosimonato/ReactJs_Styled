f7c4a29839eb5371c60061856927a4db95a96a95
leandrosimonato@gmail.com <leandrosimonato@gmail.com>
2021-04-23 12:08:27 -0300

    ReactJs_Styled

---
+++ src/Components/ImageFilter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import alimentacao from "../assets/images/alimentacao.svg";
+import outros from "../assets/images/outros.svg";
+import saude from "../assets/images/saude.svg";
+import transporte from "../assets/images/transporte.svg";
+import utilidades from "../assets/images/utilidades.svg";
+import { IconeTema } from "../Components/UI";
+
+export default (type) => {
+  const Images = {
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+    Saude: <IconeTema src={saude} alt="Saude" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    default: <IconeTema src={outros} alt="Outro" />,
+  };
+
+  return Images[type] || Images.default;
+};
